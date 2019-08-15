ae232c1d68d8a65afc93f763994d2b818b214730
Hessein Nazemi <NazemiH@safarayaneh.local>
2019-08-15 12:57:35 +0430

    playing

--- form-b/src/pages/Index.vue
+++ form-b/src/pages/Index.vue
@@ -1,14 +1,18 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
   </q-page>
 </template>
 
-<style>
-</style>
+
 
 <script>
+import Quasar from "quasar";
+Vue.use(Quasar);
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  created() {
+    console.log(Quasar);
+  }
+};
 </script>
