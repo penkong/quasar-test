24aa25048c25cf3e2492c07c626b329d8930f75e
Hessein Nazemi <NazemiH@safarayaneh.local>
2019-08-15 11:11:24 +0430

    make starter for 2 form to load up in each other

---
+++ tester/src/pages/user.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <q-card bordered dark>
+      <q-card-title>Card Title</q-card-title>
+      <q-card-separator />
+      <q-card-main>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor consequuntur tenetur libero voluptas porro aperiam quis. Similique vero maxime consequatur distinctio eum sequi minus voluptatum ratione pariatur sint fuga, hic iste minima amet, nam, cum dignissimos asperiores nesciunt veritatis esse consequuntur vel saepe. Officiis nemo voluptatem pariatur numquam, obcaecati asperiores?</q-card-main>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+};
+</script>
+
+<style lang="stylus" scoped></style>
